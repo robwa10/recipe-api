12de3ff6ed951dfc7a492d8d6b25ea997ff74523
Robert Hubbard <rob.c.hubbard@gmail.com>
2021-06-28 16:25:28 +0100

    Add tag create endpoint

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -59,3 +59,19 @@ class PrivateTagApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        payload = {'name': 'Test Tag'}
+        self.client.post(TAGS_URL, payload)
+
+        tag_exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(tag_exists)
+
+    def test_create_tag_invalid(self):
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
