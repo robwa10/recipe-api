a180bbde0990befd9c8b2c1a3db2d6af44170ef5
Robert Hubbard <rob.c.hubbard@gmail.com>
2021-09-08 16:07:52 +0100

    Add filtering for assigned_only

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Tag
+from core.models import Recipe, Tag
 
 from recipe.serializers import TagSerializer
 
@@ -14,9 +14,28 @@ TAGS_URL = reverse('recipe:tag-list')
 SAFE_EMAIL = 'test@email.com'
 SAFE_PASSWORD = 'test123'
 
+SAFE_TAG_NAME = 'Breakfast'
+SAFE_TIME_MINUTES = 10
+SAFE_TITLE = 'Sample Recipe'
+SAFE_PRICE = 5.00
+
 
 def create_sample_user(email=SAFE_EMAIL, password=SAFE_PASSWORD):
     return get_user_model().objects.create_user(email, password)
+
+
+def create_sample_recipe(user, **params):
+    defaults = {
+        'title': SAFE_TITLE,
+        'time_minutes': SAFE_TIME_MINUTES,
+        'price': SAFE_PRICE
+    }
+    defaults.update(params)
+    return Recipe.objects.create(user=user, **defaults)
+
+
+def create_sample_tag(user, name=SAFE_TAG_NAME):
+    return Tag.objects.create(user=user, name=name)
 
 
 class PublicTagsApiTests(TestCase):
@@ -75,3 +94,31 @@ class PrivateTagApiTests(TestCase):
         res = self.client.post(TAGS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_tags_assigned_to_recipes(self):
+        tag1 = create_sample_tag(user=self.user)
+        tag2 = create_sample_tag(user=self.user, name='Lunch')
+        recipe = create_sample_recipe(user=self.user)
+        recipe.tags.add(tag1)
+
+        res = self.client.get(TAGS_URL, {'assigned_only': 1})
+
+        serializer1 = TagSerializer(tag1)
+        serializer2 = TagSerializer(tag2)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertNotIn(serializer2.data, res.data)
+
+    def test_retrieve_tags_assigned_unique(self):
+        tag = create_sample_tag(user=self.user)
+        create_sample_tag(user=self.user, name='Lunch')
+
+        recipe1 = create_sample_recipe(user=self.user)
+        recipe2 = create_sample_recipe(user=self.user, title='Lunch Recipe')
+
+        recipe1.tags.add(tag)
+        recipe2.tags.add(tag)
+
+        res = self.client.get(TAGS_URL, {'assigned_only': 1})
+
+        self.assertEqual(len(res.data), 1)
